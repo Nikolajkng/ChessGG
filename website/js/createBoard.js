4dc3df3591943667e25c4dfc7a99615716b3b330
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-02 09:14:56 +0200

    changes

--- website/js/createBoard.js
+++ website/js/createBoard.js
@@ -3,11 +3,13 @@ let myDiv = document.getElementById("board");
 
 // Build the board of buttons:
 const size = 8;
-for (let x = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
+for (let x = 1; x <= size; x++) {
+    for (let y = 1; y <= size; y++) {
         let newButton = document.createElement('button');
         let newText = document.createTextNode("button: ("+x+", "+y+")" );
         newButton.appendChild(newText);
+        newButton.style.width = 100+"px";
+        newButton.style.height = 100+"px";
         myDiv.appendChild(newButton);
     }
 }
