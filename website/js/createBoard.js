a03ffebba0f4e7e7ef5dad45a40c99f7a9ce9386
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-08 23:38:42 +0200

    grid structure fixed

--- website/js/createBoard.js
+++ website/js/createBoard.js
@@ -1,5 +1,6 @@
 // Fetch the div where the board is:
 let myDiv = document.getElementById("board");
+let cellSize = 100;
 
 // Build the board of buttons:
 const size = 8;
@@ -8,8 +9,10 @@ for (let x = 1; x <= size; x++) {
         let newButton = document.createElement('button');
         let newText = document.createTextNode("button: ("+x+", "+y+")" );
         newButton.appendChild(newText);
-        newButton.style.width = 100+"px";
-        newButton.style.height = 100+"px";
+        newButton.style.width = cellSize+"px";
+        newButton.style.height = cellSize+"px";
+        newButton.style.padding = 0+"px";
+        newButton.style.margin = 0+"px";
         myDiv.appendChild(newButton);
     }
 }
