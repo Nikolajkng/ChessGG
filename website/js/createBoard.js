fba2e5de0278f34b4bc3090e61bc8a5c16b4b634
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-09 00:28:49 +0200

    added onclicker function

--- website/js/createBoard.js
+++ website/js/createBoard.js
@@ -6,13 +6,25 @@ let cellSize = 100;
 const size = 8;
 for (let x = 1; x <= size; x++) {
     for (let y = 1; y <= size; y++) {
-        let newButton = document.createElement('button');
-        let newText = document.createTextNode("button: ("+x+", "+y+")" );
-        newButton.appendChild(newText);
-        newButton.style.width = cellSize+"px";
-        newButton.style.height = cellSize+"px";
-        newButton.style.padding = 0+"px";
-        newButton.style.margin = 0+"px";
+        let coord = "("+x+", "+y+")";
+        let newButton = document.createElement('BUTTON');
+        let newText = document.createTextNode(coord);
+        styleButtons(newButton, newText);
+        newButton.addEventListener('click', () => {
+            console.log('clicked on ' + coord);
+        })
+        document.querySelector('body').appendChild(newButton);
+        
         myDiv.appendChild(newButton);
     }
 }
+
+// Style of buttons:
+function styleButtons(newButton, newText){
+    newButton.appendChild(newText);
+    newButton.style.width = cellSize+"px";
+    newButton.style.height = cellSize+"px";
+    newButton.style.padding = 0+"px";
+    newButton.style.margin = 0+"px";
+}
+
