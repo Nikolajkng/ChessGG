b295c138049db3a14b9b2d919e020e1b85b71406
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-11 22:53:12 +0200

    refactor + symbol pieces onboard

--- website/js/cellController.js
+++ website/js/cellController.js
@@ -1,74 +1,127 @@
 // Local variables:
 let cellSize = 100;
 
-// Change value of cells:
-const changeValue = (btn, value) => {
-    btn.setAttribute("id", value)
+
+// Style of buttons:
+const setCellSize = (newButton) => {
+    newButton.style.width = cellSize + "px";
+    newButton.style.height = cellSize + "px";
+    newButton.style.padding = 0 + "px";
+    newButton.style.margin = 0 + "px";
 }
 
 // Set value of cells:
-const setStartValues = (btn, x, y) => {
+const setInitialPieceValues = (btn, x, y) => {
     // Pawns
-    if (x == 2) {
-        btn.setAttribute("value", "whitepawns");
-    } else if (x == 7) {
-        btn.setAttribute("value", "blackpawns");
+    if (x == 7) {
+        btn.setAttribute("value", "whiteP");
+    } else if (x == 2) {
+        btn.setAttribute("value", "blackP");
     }
     // Rooks
     else if ((x == 8 && y == 1) || (x == 8 && y == 8)) {
-        btn.setAttribute("value", "whiterooks");
+        btn.setAttribute("value", "whiteR");
     } else if ((x == 1 && y == 1) || (x == 1 && y == 8)) {
-        btn.setAttribute("value", "blackrooks");
+        btn.setAttribute("value", "blackR");
     }
     // Knight
     else if ((x == 8 && y == 2) || (x == 8 && y == 7)) {
-        btn.setAttribute("value", "whiteknight");
+        btn.setAttribute("value", "whiteN");
     } else if ((x == 1 && y == 2) || (x == 1 && y == 7)) {
-        btn.setAttribute("value", "blackknight");
+        btn.setAttribute("value", "blackN");
     }
     // Bishop
     else if ((x == 8 && y == 3) || (x == 8 && y == 6)) {
-        btn.setAttribute("value", "whitebishop");
+        btn.setAttribute("value", "whiteB");
     } else if ((x == 1 && y == 3) || (x == 1 && y == 6)) {
-        btn.setAttribute("value", "blackbishop");
+        btn.setAttribute("value", "blackB");
     }
     // Queen
     else if ((x == 8 && y == 4)) {
-        btn.setAttribute("value", "whitequeen");
+        btn.setAttribute("value", "whiteQ");
     } else if ((x == 1 && y == 4)) {
-        btn.setAttribute("value", "blackqueen");
+        btn.setAttribute("value", "blackQ");
     }
     // King
-    else if ((x == 8 && y == 5)) {
-        btn.setAttribute("value", "whiteking");
-    } else if ((x == 1 && y == 5)) {
-        btn.setAttribute("value", "whiteking");
+    else if ((x == 1 && y == 5)) {
+        btn.setAttribute("value", "blackK");
+    } else if ((x == 8 && y == 5)) {
+        btn.setAttribute("value", "whiteK");
     }
     // Error handling to console
     else {
         btn.setAttribute("value", "none")
     }
 
-    console.log("Piece ("+x+","+y+"): "+btn.getAttribute("value"));
+    console.log("Piece (" + x + "," + y + "): " + btn.getAttribute("value"));
 }
 
 
+// Place all initial pieces based on their values:
+const setInitialPieces = (btn) => {
+    var piece = document.createTextNode("");
+    var value = btn.getAttribute('value');
+    switch (value) {
+        case value = "whiteP":
+            piece.nodeValue = "♙";
+            break;
+
+        case value = "whiteK":
+            piece.nodeValue = "♔";
+            break;
+
+        case value = "whiteQ":
+            piece.nodeValue = "♕";
+            break;
+
+        case value = "whiteR":
+            piece.nodeValue = "♖";
+            break;
+
+        case value = "whiteB":
+            piece.nodeValue = "♗";
+            break;
+
+        case value = "whiteN":
+            piece.nodeValue = "♘";
+            break;
+        case value = "blackP":
+            piece.nodeValue = "♟";
+            break;
+
+        case value = "blackK":
+            piece.nodeValue = "♚";
+            break;
+
+        case value = "blackQ":
+            piece.nodeValue = "♛";
+            break;
+
+        case value = "blackR":
+            piece.nodeValue = "♜";
+            break;
+
+        case value = "blackB":
+            piece.nodeValue = "♝";
+            break;
+
+        case value = "blackN":
+            piece.nodeValue = "♞";
+            break;
+
+        default:
+            piece.nodeValue = "";
+    }
+    btn.appendChild(piece);
+}
+
 // Onclick function of buttons:
-const onClick = (newButton, coord) => {
-    newButton.addEventListener('click', () => {
+const onClick = (btn, coord) => {
+    btn.addEventListener('click', () => {
         console.log('clicked on ' + coord);
 
         // Changes value of cells after click:
-        changeValue(newButton, "-1");
-        console.log(newButton.getAttribute("value"))
+        btn.setAttribute('value', "selected");
+        console.log(btn.getAttribute("value"));
     })
-}
-
-// Style of buttons:
-const styleCells = (newButton, newText) => {
-    newButton.appendChild(newText);
-    newButton.style.width = cellSize + "px";
-    newButton.style.height = cellSize + "px";
-    newButton.style.padding = 0 + "px";
-    newButton.style.margin = 0 + "px";
 }
--- website/js/createBoard.js
+++ website/js/createBoard.js
@@ -6,14 +6,14 @@ const size = 8;
 for (let x = 1; x <= size; x++) {
     for (let y = 1; y <= size; y++) {
         let coord = "("+x+", "+y+")";
-        let newButton = document.createElement('button');
-        //let newLabel = document.createTextNode(coord);
-        setStartValues(newButton, x, y);
-        styleCells(newButton, newLabel);
-        onClick(newButton, coord);
+        let btn = document.createElement('button');
+        setInitialPieceValues(btn, x, y);
+        setInitialPieces(btn)
+        setCellSize(btn);
+        onClick(btn, coord);
         
-        document.querySelector('body').appendChild(newButton);
-        myDiv.appendChild(newButton);
+        document.querySelector('body').appendChild(btn);
+        myDiv.appendChild(btn);
     }
 }
 
