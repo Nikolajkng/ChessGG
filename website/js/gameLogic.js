2243f15bc9b0639a8985be20b594499ecf777a7d
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-25 23:11:25 +0200

    remove highlight on move fixed

--- website/js/gameLogic.js
+++ website/js/gameLogic.js
@@ -2,7 +2,14 @@ let canSelect = true;
 let selectedPos = "";
 let selectedX = "";
 let selectedY = "";
+let selectedBtn = "";
 let playerTurn = 0;
+
+const removeHighlight = () => {
+    selectedBtn.classList.remove('highlight');
+    selectedBtn.setAttribute('selected', 'no');
+    canSelect = true;
+}
 
 const selectedCell = (btn, x, y) => {
     cellType = btn.getAttribute('value');
@@ -13,6 +20,7 @@ const selectedCell = (btn, x, y) => {
 
     // Ensures one selection at a time on cells containing pieces only
     if (canSelect && cellType != 'none' && (btn.getAttribute('value').includes('white'))) {
+        selectedBtn = btn;
         selectedX = x;
         selectedY = y;
         selectedPos = selectedX + "" + selectedY;
@@ -22,9 +30,7 @@ const selectedCell = (btn, x, y) => {
     } 
     // Remove selection only when clicking on same selected cell
     else if (!canSelect && selectedPos == (x + "" + y)) {
-        btn.classList.remove('highlight');
-        btn.setAttribute('selected', 'no');
-        canSelect = true;
+        removeHighlight();
     }
 }
 
@@ -46,23 +52,33 @@ const onClick = (btn, x, y) => {
         console.log('Coordinates: ' + "("+x+", "+y+")");
         console.log('Value: ' + btn.getAttribute("value"));
         console.log('Selected: ' + btn.getAttribute("selected"));
-        console.log("|---------------------|")
+        console.log("#################################################")
     })
 }
 
 // Move piece
 const movePiece = (x, y) => {
-    let mySelectPos = selectedX+""+selectedY;
+    let oldPos = selectedX+""+selectedY;
     let newPos = x+""+y;
-    
     let moveDetected = Math.abs(selectedX - x) != 0 || Math.abs(selectedY - y) != 0 
+
     if(moveDetected){
+
+        // Remove highlight on movement on oldPos:
+        removeHighlight();
+
+        // Swap piece position and set their coordinates accordingly:
+        
+
         playerTurn++;
+        console.log("|---------------------------------|");
+        console.log("new position: " + newPos);
+        console.log("old position: " + oldPos);
         console.log("movement detected: " + playerTurn);
-        console.log("#######################################################");
+        console.log("|---------------------------------|");
     } else {
         console.log("no movement detected");
-        console.log("#######################################################");
+        console.log("|---------------------------------|");
     }
     
 }
