4188e10498890dbb47e2cefc0c037fd604cfd38b
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-25 22:54:48 +0200

    added playerTurn + starting on movePiece

--- website/js/gameLogic.js
+++ website/js/gameLogic.js
@@ -1,12 +1,21 @@
 let canSelect = true;
 let selectedPos = "";
+let selectedX = "";
+let selectedY = "";
+let playerTurn = 0;
 
 const selectedCell = (btn, x, y) => {
     cellType = btn.getAttribute('value');
 
+    // TODO:
+    // for 2 player (Check who is playing, host or client, and correct the "include.white")
+
+
     // Ensures one selection at a time on cells containing pieces only
-    if (canSelect && cellType != 'none') {
-        selectedPos = x + "" + y;
+    if (canSelect && cellType != 'none' && (btn.getAttribute('value').includes('white'))) {
+        selectedX = x;
+        selectedY = y;
+        selectedPos = selectedX + "" + selectedY;
         btn.setAttribute('selected', 'yes')
         btn.classList.add('highlight');
         canSelect = false;
@@ -27,7 +36,7 @@ const onClick = (btn, x, y) => {
         selectedCell(btn, x, y);
 
         // Move piece logic (TODO):
-        
+        movePiece(x, y);
 
         // Remove piece logic (TODO):
 
@@ -39,4 +48,21 @@ const onClick = (btn, x, y) => {
         console.log('Selected: ' + btn.getAttribute("selected"));
         console.log("|---------------------|")
     })
+}
+
+// Move piece
+const movePiece = (x, y) => {
+    let mySelectPos = selectedX+""+selectedY;
+    let newPos = x+""+y;
+    
+    let moveDetected = Math.abs(selectedX - x) != 0 || Math.abs(selectedY - y) != 0 
+    if(moveDetected){
+        playerTurn++;
+        console.log("movement detected: " + playerTurn);
+        console.log("#######################################################");
+    } else {
+        console.log("no movement detected");
+        console.log("#######################################################");
+    }
+    
 }
