fd73f4575672d1064438c32c0fe6a03426c0997b
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-28 18:48:20 +0200

    moved piece logic to their own files

--- website/js/pawnRules.js
+++ website/js/pawnRules.js
@@ -1,33 +1,3 @@
-const legalMove = (selectedBtn, targetBtn) => {
-
-    if (selectedBtn != null) {
-
-        const targetValue = targetBtn.getAttribute("value");
-        const selectValue = selectedBtn.getAttribute("value");
-        console.log("checking legal move for: " + selectValue);
-        console.log("target move is: " + targetValue)
-
-        if (selectValue.includes("P")) {
-            return pawnRules(selectedBtn, targetBtn);
-        } else if (selectValue.includes("N")) {
-            return knightRules(selectedBtn, targetBtn);
-        } else if (selectValue.includes("B")) {
-            return bishopRules(selectedBtn, targetBtn);
-        } else if (selectValue.includes("R")) {
-            return rookRules(selectedBtn, targetBtn);
-        } else if (selectValue.includes("K")) {
-            return kingRules(selectedBtn, targetBtn);
-        } else if (selectValue.includes("Q")) {
-            return queenRules(selectedBtn, targetBtn);
-        }
-
-        return false;
-    } else {
-        // TO DO: add chess illegal move sound
-        console.error("Error: Selectedbtn === null -> Cause: (1): not your turn, (2): not your pieces");
-    }
-}
-
 
 const pawnRules = (mySelectBtn, targetBtn) => {
 
@@ -89,22 +59,4 @@ const pawnRules = (mySelectBtn, targetBtn) => {
         // Error handling:
         console.error("Error in pawnRules");
     };
-}
-
-
-// TO DO: 
-const knightRules = (mySelectBtn, targetBtn) => {
-    return true;
-}
-const bishopRules = (mySelectBtn, targetBtn) => {
-    return true;
-}
-const rookRules = (mySelectBtn, targetBtn) => {
-    return true;
-}
-const kingRules = (mySelectBtn, targetBtn) => {
-    return true;
-}
-const queenRules = (mySelectBtn, targetBtn) => {
-    return true;
 }
