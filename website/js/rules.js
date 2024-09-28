79a47624b58b97b8bbbc172906ed32138d4f866f
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-28 15:00:44 +0200

    pawn logic fixed

--- website/js/rules.js
+++ website/js/rules.js
@@ -1,22 +1,18 @@
-
 const legalMove = (selectedBtn, targetBtn) => {
 
-    if(selectedBtn != null){
-        
-    const targetValue = targetBtn.getAttribute("value");
-    const selectValue = selectedBtn.getAttribute("value");
+    if (selectedBtn != null) {
 
-   switch (selectValue){
-        case "whiteP": 
-        pawnRules(selectedBtn, targetBtn);
-        break;
+        const targetValue = targetBtn.getAttribute("value");
+        const selectValue = selectedBtn.getAttribute("value");
+        console.log("checking legal move for: " + selectValue);
+        console.log("target move is: " + targetValue)
 
-    }
+        if (selectValue.includes("P")) {
+            return pawnRules(selectedBtn, targetBtn);
+        }
 
-    console.log("checking legal move for: " + selectValue);
-    console.log("target move is: " + targetValue )
 
-    return true;
+        return false;
     } else {
         // TO DO: add chess illegal move sound
         console.error("Error: Selectedbtn === null -> Cause: (1): not your turn, (2): not your pieces");
@@ -24,6 +20,11 @@ const legalMove = (selectedBtn, targetBtn) => {
 
 }
 
-const pawnRules = () => {
-
+const pawnRules = (mySelectBtn, targetBtn) => {
+    const y = parseInt(mySelectBtn.getAttribute("x"));
+    const tY = parseInt(targetBtn.getAttribute("x"));
+    const diff = Math.abs(y - tY);
+    console.log("SE MIG: " + targetBtn.getAttribute("value"));
+    const noCapture = targetBtn.getAttribute("value") === "none";
+    return (diff >= 0 && diff <= 2) && noCapture;
 }
