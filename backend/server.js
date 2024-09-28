89235086200ba09e03b8cf97054d36cf119b5681
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-28 21:55:52 +0200

    faviicon added + sorted css styles + socket is wokring

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 /////////////////////// imports //////////////////////////
-
-const express = require('express');
-const path = require('path')
-const app = express();
-
 const hostname = '127.0.0.1';
 const port = 6969;
 
+const path = require('path')
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
 //////////////////////// setup /////////////////////////////////
 
-//Links the server to HTML file:
+// Links the server to HTML file:
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../website', 'index.html'))
 });
@@ -18,8 +21,22 @@ app.get('/', function (req, res) {
 app.use(express.static(path.join(__dirname, '../website')));
 
 
+// Trigger an series of actions on connection:
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.emit('connection', {message: 'a new client connected'});
+
+
+    // Disconnection handler
+    socket.on('disconnection', function () {
+        console.log("user disconnected");
+    })
+
+})
+
+
 // Server checks:
-app.listen(port, function () {
+server.listen(port, function () {
     console.log(`Server running at http://${hostname}:${port}/`);
 
 });
