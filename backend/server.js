d42fdb2f29e41c805f2f99ac1f47e6913c39d6f6
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-09-29 22:46:48 +0200

    added player connection handling + name showing in chatbox

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 /////////////////////// imports //////////////////////////
 const port = 6969;
+const players = {};
+
 const path = require('path')
 const express = require('express');
 const app = express();
-
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
@@ -21,13 +22,17 @@ app.use(express.static(path.join(__dirname, '../website')));
 
 // Triggers a series of actions and channel-listeners on client connection:
 io.on('connection', (socket) => {
-    console.log('user connected!');
 
+    // Listening for new players...
+    socket.on('new-player', playerName => {
+        players[socket.id] = playerName;
+        socket.broadcast.emit('user-connected', playerName);
+    })
 
-    // Listening for client...
+    // Listening for new chat messages...
     socket.on('send-chat-message', message => {
         console.log(message);
-        // Sending from server to client...
+        // Send to all clients
         socket.broadcast.emit('chat-message', message);
     })
 
