752463722785c4d1ebf196541b57a7b745fe7d3c
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-10-03 22:07:39 +0200

    fixed missing curly brackets

--- website/js/rules/rookRules.js
+++ website/js/rules/rookRules.js
@@ -1,34 +1,30 @@
 const rookRules = (mySelectBtn, targetBtn) => {
-    
+
     // Relative Positions and info:
     const x = parseInt(mySelectBtn.getAttribute("x"));
     const y = parseInt(mySelectBtn.getAttribute("y"));
-    
+
     const tX = parseInt(targetBtn.getAttribute("x"));
     const tY = parseInt(targetBtn.getAttribute("y"));
-    
+
 
     if (tX - x !== 0) { // har rykket vandret 
         for (let j = minY + 1; j < maxY; j++) {
             if (chessBoard[x][j] !== "none") {
                 return false; // Something is blocking the rook's path
-        
-    } 
-    
-    else if(tY - y !== 0){ // har rykket lodret
-        
+
+            } else if (tY - y !== 0) { // har rykket lodret
+
+            } else {
+                return false //har rykket diagonalt
+            }
+        }
     }
 
-    else {
-        return false //har rykket diagonalt
-    }
-
-
-    for (let i = x ; i < tX; i++){ // tjekker vandret
+    for (let i = x; i < tX; i++) { // tjekker vandret
         if (chessBoard[i][y] === "none") {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
@@ -36,12 +32,11 @@ const rookRules = (mySelectBtn, targetBtn) => {
     for (let j = y; j > tY; j++) { // tjekker lodret
         if (chessBoard[j][y] === "none") {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-        
+
     /*
     let chessBoard = [
     [null, null, null, null, null, null, null, null],
@@ -61,7 +56,4 @@ man skal tænke på, at den kun kan rykke i array
   myselect og target skal bruges.
     */
 
-
-    // IF RULES FOR ROOK IS OK = Return true! (se pawnRules)
-    return true
 }
