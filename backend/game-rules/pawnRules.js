a0587df2b85a4b804e9f0b606c8a05e10572cc18
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-10-04 22:06:07 +0200

    implemented arrayupdater for pawns

--- backend/game-rules/pawnRules.js
+++ backend/game-rules/pawnRules.js
@@ -15,10 +15,12 @@ function pawnRules(x, y, tX, tY, chessBoard, turn, sValue, tValue) {
     // Shared rules
     let straightMove = Math.abs(y - tY) === 0;
     let maxStep = ""
+    let firstMove = false;
 
     // Initial position allow pawns to move 2 steps
     if (x == 7 || x == 2) {
         maxStep = Math.abs(x - tX) <= 2;
+        firstMove = true;
     } else {
         // Exit of intial restricts move to 1 step
         maxStep = Math.abs(x - tX) === 1;
@@ -27,13 +29,55 @@ function pawnRules(x, y, tX, tY, chessBoard, turn, sValue, tValue) {
 
     if (turn === "White") {
         // Specific rules for white
-        let whiteFreePath = chessBoard[x - 1][y] === "none" || chessBoard[x - 2][y] === "none";
+        let whiteFreePath = ""
+        if (firstMove) {
+            whiteFreePath = chessBoard[x - 1][y] && chessBoard[x - 2][y] === "none";
+            firstMove = false;
+        } else {
+            chessBoard[x - 1][y] === "none"
+        }
+
         let whiteGoBackwards = tX > x;
-        return (whiteFreePath && straightMove && maxStep && !whiteGoBackwards)
+        let satisfyAllRulesWhite = (whiteFreePath && straightMove && maxStep && !whiteGoBackwards);
+
+        if (satisfyAllRulesWhite) {
+            swapValueArray(chessBoard, x, y, tX, tY)
+        }
+
+        return satisfyAllRulesWhite;
     } else if (turn === "Black") {
         // Specific rules for black
-        let blackFreePath = chessBoard[x + 1][parseInt(y)] === "none" || chessBoard[x + 2][y] === "none";
+        if (firstMove) {
+            blackFreePath = chessBoard[x + 1][y] && chessBoard[x + 2][y] === "none";
+            firstMove = false;
+        } else {
+            chessBoard[x - 1][y] === "none"
+        }
         let blackGoBackwards = tX < x;
-        return (blackFreePath && straightMove && maxStep && !blackGoBackwards);
+        let satisfyAllRulesBlack = (blackFreePath && straightMove && maxStep && !blackGoBackwards);
+        if (satisfyAllRulesBlack) {
+            swapValueArray(chessBoard, x, y, tX, tY)
+        }
+
+        return satisfyAllRulesBlack;
     }
 }
+
+function swapValueArray(chessBoard, x, y, tX, tY) {
+    const temp = chessBoard[x-1][y-1];
+    chessBoard[x-1][y-1] = chessBoard[tY-1][tX-1]
+    chessBoard[tX-1][tY-1] = temp;
+
+    // Check array
+    printBoard(chessBoard)
+};
+
+function printBoard(chessBoard){
+    console.log("\n")
+    console.log("---------------------------------------------------------------------")
+    chessBoard.forEach(row => {
+        console.log(row.join(" | "))
+    });
+    console.log("---------------------------------------------------------------------")
+    console.log("\n")
+};
